67853fd6c92bfaf7ab1549dd2fbf186983a8bdf5
Ludwik Bielczynski <ludwik.bielczynski@yieldplanet.com>
2020-07-29 17:40:50 +0200

    Expend front end for relay labels

--- android_app/main.py
+++ android_app/main.py
@@ -6,6 +6,7 @@ from kivy.uix.button import Button
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.switch import Switch
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
@@ -15,9 +16,6 @@ relays = [{'nr': 1, 'start': '18:00', 'end': '20:00', 'weekdays': ['Mo']},
           {'nr': 3, 'start': '18:00', 'end': '20:00', 'weekdays': ['Tu', 'Fr']},
          ]
 # relays = []
-
-class WeekdaysButton(Button):
-  pass
 
 class RelayControlerWidget(GridLayout):
 
@@ -37,8 +35,8 @@ class RelayControlerWidget(GridLayout):
       weekdays += f'{weekday}, '
 
     weekdays = weekdays[:-2]
-
-    self.add_widget(WeekdaysButton(text=weekdays))
+    self.weekday_button = Button(text=weekdays)
+    self.add_widget(self.weekday_button)
 
     # Create checkbox/radio button to trigger manual override
     self.manual_checkbox = CheckBox()
@@ -49,24 +47,38 @@ class RelayControlerWidget(GridLayout):
     # manual_layout
     # self.add_widget(self.manual_checkbox)
 
+class TopLabels(BoxLayout):
+
+  def __init__(self, **kwargs):
+    super(TopLabels, self).__init__(**kwargs)
+    self.orientation = 'horizontal'
+    self.cols = 5
+    self.height = 0.1
+    self.add_widget(Label(text='Section'))
+    self.add_widget(Label(text='Start'))
+    self.add_widget(Label(text='End'))
+    self.add_widget(Label(text='Weekday'))
+    self.add_widget(Label(text='Manual'))
+
 class RelayControlersLayout(StackLayout):
 
   def __init__(self, **kwargs):
     super(RelayControlersLayout, self).__init__(**kwargs)
-
+    self.add_widget(TopLabels(size_hint=(1, 0.06), spacing=(0, 0),))
+    self.relays_control_widgets = []
     for relay in relays:
       relay_controler_widget = RelayControlerWidget(relay,
-                                                    size_hint=(1, 0.15),
+                                                    size_hint=(1, 0.06),
                                                     spacing=(0, 0),
                                                    )
-
+      self.relays_control_widgets.append(relay_controler_widget)
       self.add_widget(relay_controler_widget)
 
 class SettingsPopupContent(GridLayout):
 
   def __init__(self, **kwargs):
     super(SettingsPopupContent, self).__init__(**kwargs)
-    self.rows = 2
+    self.rows = 3
 
     self.add_widget(Label(text='User Name'))
     self.username = TextInput(multiline=False)
@@ -76,27 +88,29 @@ class SettingsPopupContent(GridLayout):
     self.password = TextInput(password=True, multiline=False)
     self.add_widget(self.password)
 
+    self.add_widget(Label(text=''))
+    self.button_login = Button(text='Log in')
+    self.add_widget(self.button_login)
+
 class SettingsPopupLayout(AnchorLayout):
 
   def __init__(self, **kwargs):
     super(SettingsPopupLayout, self).__init__(anchor_x='right', anchor_y='bottom', **kwargs)
     settings_popup_content = SettingsPopupContent()
     self.popup = Popup(title='Settings',
-                  content=settings_popup_content,
-                  size_hint=(None, None),
-                  size=(400, 150),
-                 )
+                       content=settings_popup_content,
+                       size_hint=(None, None),
+                       size=(Window.width*0.9, Window.height*0.25),
+                      )
 
 class RootScreen(GridLayout):
 
   def __init__(self, **kwargs):
     super(RootScreen, self).__init__(**kwargs)
-
-    self.set_content(Window.width, Window.height)
-
-  def set_content(self, width, height, *args):
-    self.rows = 2
-
+    # self.orientation = 'vertical'
+    # self.spacing = [0, 0]
+    # self.padding = [0, 0]
+    self.rows = 3
     popup_layout = SettingsPopupLayout()
     self.settings_button = Button(text='>',
                                   size_hint=(None, None),
