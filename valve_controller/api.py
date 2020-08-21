61c3c9409a39b38c0f7b9bd8cad50b6b7ed42c26
Ludwik Bielczynski <ludwik.bielczynski@yieldplanet.com>
2020-08-21 20:06:25 +0200

    Working on basic example

--- valve_controller/api.py
+++ valve_controller/api.py
@@ -2,18 +2,28 @@ import time
 
 import RPi.GPIO as GPIO
 
-if __name__ == '__main__':
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(20, GPIO.OUT)
-    GPIO.setup(21, GPIO.OUT)
-    GPIO.setup(26, GPIO.OUT)
+RELAY_CHANNELS_BCM_MAP = {1: 26,
+                          2: 20,
+                          3: 21
+                         }
 
+if __name__ == '__main__':
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    for pin in RELAY_CHANNELS_BCM_MAP.values():
+        GPIO.setup(pin, GPIO.OUT)
+
+    print('Setup The Relay Module is successful')
     try:
         while True:
-            for pin in [20, 21, 26]:
-                GPIO.output(20, True)
+            for channel, pin in RELAY_CHANNELS_BCM_MAP.items():
+                GPIO.output(pin, GPIO.LOW)
+                print(f'Channel {channel} on pin {pin} is on')
                 time.sleep(1)
-                GPIO.output(21, False)
+                GPIO.output(pin, GPIO.HIGH)
+    except Exception as message:
+        print(f'An exception occurred: {message}')
 
     finally:
         GPIO.cleanup()
