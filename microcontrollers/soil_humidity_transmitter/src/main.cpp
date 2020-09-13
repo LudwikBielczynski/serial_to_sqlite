c7e6a323ac0827d78b380e5a854dfed6a4b6a7ac
Ludwik Bielczynski <ludwik.bielczynski@yieldplanet.com>
2020-09-13 13:30:35 +0200

    [soil_huimidity_transmitter] Powering modules on and off working well

--- microcontrollers/soil_humidity_transmitter/src/main.cpp
+++ microcontrollers/soil_humidity_transmitter/src/main.cpp
@@ -6,15 +6,16 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
 #include <printf.h>
-
+#include <avr/sleep.h>
+#include <avr/power.h>
 
 // Pin layout
 const uint8_t RADIO_POWER_PIN = 4;
 const uint8_t SOIL_HUMIDITY_POWER_PIN = 5;
 const uint8_t CE_PIN = 8;
 const uint8_t CSN_PIN = 9;
+const uint8_t SCK_PIN = 13; // default SCK pin
 const uint8_t SENSOR_PIN = 14; // A0
 const uint8_t VOLTAGE_SPLITTER_PIN = 16; // A2
 
@@ -23,15 +24,15 @@ RF24 radio(CE_PIN, CSN_PIN);
 const byte SLAVE_ADDRESS[5] = {'R','x','0','0','1'};
 
 const char * TRANSMITTER_NAME = "Tx1";
-// const unsigned long TRANSMITTER_SEND_INTERVAL_MS = 5000; // Once per 5sec
-const unsigned long long TRANSMITTER_SEND_INTERVAL_MS = 1800000; // Once per 30min
+const unsigned long TRANSMITTER_SEND_INTERVAL_MS = 1000; // Once per 5sec
+// const unsigned long long TRANSMITTER_SEND_INTERVAL_MS = 1800000; // Once per 30min
 bool shouldStartWithMeasurement = true;
 unsigned long long previousTimeMs;
 unsigned long long currentTimeMs;
 bool isLargerThanInterval = false;
 
 // Soil humidity settings
-const unsigned short int SOIL_MOISTURE_MEASUREMENTS_NR = 4;
+const unsigned short int SOIL_MOISTURE_MEASUREMENTS_NR = 1;
 const unsigned short dataToSendSize = 12;
 int soilHumiditySensorValue;
 
@@ -44,10 +45,14 @@ int voltagesSum = 0;
 float voltage;
 
 
-void setUpTransmitter() {
+void transmitterSetUp() {
   Serial.println("Starting the transmitter...");
 
+  pinMode(CE_PIN, INPUT);
+  pinMode(SCK_PIN, OUTPUT);
+
   bool isSetupSuccess = radio.begin();
+  radio.powerUp();
   Serial.print("radio.begin() success: ");
   Serial.println(isSetupSuccess);
 
@@ -62,7 +67,7 @@ void setUpTransmitter() {
     radio.setDataRate(RF24_250KBPS);
     radio.setPALevel(RF24_PA_MIN);
 
-    radio.printDetails();
+    // radio.printDetails();
     Serial.println("Ready to transmit...");
   }
   else
@@ -77,11 +82,10 @@ void setup() {
   printf_begin();
   pinMode(RADIO_POWER_PIN, OUTPUT);
   pinMode(SOIL_HUMIDITY_POWER_PIN, OUTPUT);
-
 }
 
 
-float measureVoltage() {
+float measureBatteryVoltage() {
   voltagesSum = 0;
   for (size_t i = 0; i < VOLTAGE_MEASUREMENTS_NR; i++)
   {
@@ -90,19 +94,19 @@ float measureVoltage() {
   }
   // Serial.println(voltagesSum / VOLTAGE_MEASUREMENTS_NR);
   float voltage = VOLTAGE_SPLIT_FACTOR * REFERENCE_VOLTAGE * voltagesSum / (1024.0 * VOLTAGE_MEASUREMENTS_NR);
+  // Serial.println(voltage);
   return voltage;
 }
 
 
-long readVoltageFromInternalRef() {
-  long result; // Read 1.1V reference against AVcc
-  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  delay(2); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Convert
-  while (bit_is_set(ADCSRA, ADSC));
-  result = ADCL; result |= ADCH<<8;
-  result = 1126400L / result; // Back-calculate AVcc in mV
-  return result;
+uint8_t measureSoilHumidity() {
+    digitalWrite(SOIL_HUMIDITY_POWER_PIN, HIGH);
+    delay(100);
+    soilHumiditySensorValue = analogRead(SENSOR_PIN);
+    // Serial.println(soilHumiditySensorValue);
+    digitalWrite(SOIL_HUMIDITY_POWER_PIN, LOW); // Power sensor down
+
+    return soilHumiditySensorValue;
 }
 
 
@@ -123,7 +127,7 @@ void prepareDataToSend(char * dataToSend, unsigned short soilHumiditySensorValue
 }
 
 
-bool send(char * dataToSend, unsigned short dataToSendSize) {
+bool transmitterSendData(char * dataToSend, unsigned short dataToSendSize) {
   Serial.println("Trying to send data");
   bool isWriteSuccess = radio.write(dataToSend, dataToSendSize);
 
@@ -140,12 +144,46 @@ bool send(char * dataToSend, unsigned short dataToSendSize) {
   if (radio.failureDetected) {
     Serial.println("A failure was detected. Trying to Reset configuration");
     radio.failureDetected = 0; // Reset the detection value
-    setUpTransmitter();
+    transmitterSetUp();
+  }
+  else{
+    Serial.println("Data sent");
   }
 
   return isWriteSuccess;
 }
 
+
+void transmitterTurnOff() {
+  radio.powerDown();
+
+  // Output pins to input so the current is not drawn
+  pinMode(CE_PIN, INPUT);
+  pinMode(SCK_PIN, INPUT);
+
+  digitalWrite(RADIO_POWER_PIN, LOW); // Switch off the low side switch transistor
+
+  Serial.println("Deactivated radio");
+}
+
+void sleep_microcontroller() {
+    Serial.println("Starting to sleep");
+
+    delay(5000);
+    // digitalWrite(SCK_PIN, LOW);
+
+    // set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+    // set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    // sleep_enable();
+    // sleep_mode();
+    // clock_prescale_set(clock_div_4);
+    // // CLKPR = 0x02;
+    // delay(3000);
+    // clock_prescale_set(clock_div_1);
+    // power_all_enable();
+    // sleep_disable();
+    Serial.println("Woke up");
+}
 
 void loop() {
   currentTimeMs = millis();
@@ -155,37 +193,27 @@ void loop() {
     for (size_t i = 0; i < SOIL_MOISTURE_MEASUREMENTS_NR; i++)
     {
       // Read battery voltage before other components are powered up
-      voltage = measureVoltage();
-      Serial.println(voltage);
+      voltage = measureBatteryVoltage();
 
-      // char dataToSend[dataToSendSize] = ""; // Important to zero this variable before preparing data
+      char dataToSend[dataToSendSize] = ""; // Important to zero this variable before preparing data
 
       // // Measure soil moisture
-      Serial.println("Measure soil humidity");
-      // digitalWrite(SOIL_HUMIDITY_POWER_PIN, HIGH);
-      delay(5000);
-
-      soilHumiditySensorValue = analogRead(SENSOR_PIN);
-      Serial.println(soilHumiditySensorValue);
-      // prepareDataToSend(dataToSend, soilHumiditySensorValue, voltage);
-
-      // digitalWrite(SOIL_HUMIDITY_POWER_PIN, LOW); // Power sensor down
+      soilHumiditySensorValue = measureSoilHumidity();
 
       // Send data to the receiver
+      prepareDataToSend(dataToSend, soilHumiditySensorValue, voltage);
+
       Serial.println("Activate radio");
-      // digitalWrite(RADIO_POWER_PIN, HIGH);
+      digitalWrite(RADIO_POWER_PIN, HIGH);
       delay(200);
 
-      // setUpTransmitter();
-      // radio.powerUp();
-      // send(dataToSend, dataToSendSize);
-      // Serial.println("Data sent");
+      transmitterSetUp();
+      transmitterSendData(dataToSend, dataToSendSize);
+      transmitterTurnOff();
 
-      /// radio.powerDown();
-      // digitalWrite(RADIO_POWER_PIN, LOW); // Power radio down
-      Serial.println("Deactivated radio");
-      delay(5000);
+      delay(2000);
     }
+    sleep_microcontroller();
 
     shouldStartWithMeasurement = false;
     previousTimeMs = millis();
