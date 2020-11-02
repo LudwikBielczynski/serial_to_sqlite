f77f825e60c402c2e2815fea2056d3f343306463
Ludwik Bielczynski <ludwik.bielczynski@yieldplanet.com>
2020-11-02 09:06:40 +0100

    [soil_humidity_transmitter] Debugging

--- microcontrollers/soil_humidity_transmitter/src/main.cpp
+++ microcontrollers/soil_humidity_transmitter/src/main.cpp
@@ -8,6 +8,7 @@
 #include <printf.h>
 
 // Pin layout
+const uint8_t MICROCONTROLLER_ON_PIN = 7;
 const uint8_t RADIO_POWER_PIN = 4;
 const uint8_t SOIL_HUMIDITY_POWER_PIN = 5;
 const uint8_t CE_PIN = 8;
@@ -30,17 +31,19 @@ const float VOLTAGE_SPLIT_FACTOR = 11.0; // based on (R1 + R2)/R2 where R1=100kO
 const float VOLTAGE_CORRECTION = 0.00;
 float voltage;
 
-Transmitter transmitter(CE_PIN, CSN_PIN, SCK_PIN, RADIO_POWER_PIN, SLAVE_ADDRESS);
+// Transmitter transmitter(CE_PIN, CSN_PIN, SCK_PIN, RADIO_POWER_PIN, SLAVE_ADDRESS);
 // SoilHumiditySensor soil_humidity_sensor(SOIL_HUMIDITY_POWER_PIN, SOIL_HUMIDITY_SENSOR_PIN);
-Microcontroller microcontroller(VOLTAGE_SPLITTER_PIN, VOLTAGE_SPLIT_FACTOR, REFERENCE_VOLTAGE, VOLTAGE_CORRECTION);
+// Microcontroller microcontroller(VOLTAGE_SPLITTER_PIN, VOLTAGE_SPLIT_FACTOR, REFERENCE_VOLTAGE, VOLTAGE_CORRECTION);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Powered up the microprocessor...");
+  pinMode(MICROCONTROLLER_ON_PIN, OUTPUT);
+  digitalWrite(MICROCONTROLLER_ON_PIN, HIGH);
 
   // analogReference(EXTERNAL);
-  analogRead(VOLTAGE_SPLITTER_PIN); // First read after switching to external reference are not reliable
-  analogRead(SOIL_HUMIDITY_SENSOR_PIN);
+  // analogRead(VOLTAGE_SPLITTER_PIN); // First read after switching to external reference are not reliable
+  // analogRead(SOIL_HUMIDITY_SENSOR_PIN);
 
   // printf_begin();
   pinMode(RADIO_POWER_PIN, OUTPUT);
@@ -54,28 +57,35 @@ void loop() {
   Serial.println(F_CPU);
 
   // Read battery voltage before other components are powered up
-  voltage = microcontroller.measureBatteryVoltage(VOLTAGE_MEASUREMENTS_NR);
-  Serial.println(voltage);
-  delay(4000);
+  // voltage = microcontroller.measureBatteryVoltage(VOLTAGE_MEASUREMENTS_NR);
+  // Serial.println(voltage);
+  delay(2000);
 
   // char dataToSend[dataToSendSize] = ""; // Important to zero this variable before preparing data
 
   // // // Measure soil moisture
 
   digitalWrite(SOIL_HUMIDITY_POWER_PIN, HIGH);
+  Serial.println("SOIL_HUMIDITY_POWER_PIN");
   delay(2000);
   digitalWrite(SOIL_HUMIDITY_POWER_PIN, LOW);
+
+  digitalWrite(RADIO_POWER_PIN, HIGH);
+  Serial.println("RADIO_POWER_PIN");
+  delay(2000);
+  digitalWrite(RADIO_POWER_PIN, LOW);
+
 
   // soilHumiditySensorValue = soil_humidity_sensor.measure();
   // Serial.println(soilHumiditySensorValue);
   // // Send data to the receiver
-  digitalWrite(RADIO_POWER_PIN, HIGH);
-  transmitter.turnOn();
+  // digitalWrite(RADIO_POWER_PIN, HIGH);
+  // transmitter.turnOn();
   // transmitter.setUp();
   // transmitter.prepareDataToSend(dataToSend, soilHumiditySensorValue, voltage);
   // transmitter.sendData(dataToSend, dataToSendSize);
-  transmitter.turnOff();
+  // transmitter.turnOff();
 
-  microcontroller.sleep();
+  // microcontroller.sleep();
 
 }
